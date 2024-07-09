a5c3ba4e79678e0ff58d982f8e52e4ed9ec337a2
Aman Pandey <aman2001mi@gmail.com>
2024-07-09 17:51:09 +0530

    working timer panels

--- debug_toolbar/panels/timer.py
+++ debug_toolbar/panels/timer.py
@@ -17,7 +17,7 @@ class TimerPanel(Panel):
     Panel that displays the time a response took in milliseconds.
     """
 
-    is_async = False
+    is_async = True
 
     def nav_subtitle(self):
         stats = self.get_stats()
